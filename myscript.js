aeec6bbec853e80245b1442bf8869279e3f1fbb5
Jan Matošík <jan.matosik@gmail.com>
2018-04-09 20:53:34 +0200

    Mark posts which already scrolled up

--- myscript.js
+++ myscript.js
@@ -1,18 +1,29 @@
 function appendMessage(el, message, color) {
     var msgElement = document.createElement("p")
     msgElement.appendChild(document.createTextNode(message))
-    el.style.border = "4px solid " + color
+    el.style.border = "2px solid " + color
 
     el.insertBefore(msgElement, el.firstChild)
 }
 
 function markAsRead() {
-    var substream = document.querySelectorAll('[id^="hyperfeed_story_id_"]:not(.seen)')
-  
-    for (var i = 0; i < substream.length; i++) {
-        var post = substream[i]
-        appendMessage(post, "Post:", "blue")
-        post.className += " seen";
+    let unobserved = document.querySelectorAll('[id^="hyperfeed_story_id_"]:not(.seen)')
+    
+    for (let i = 0; i < unobserved.length; i++) {
+        let post = unobserved[i]
+        observer.observe(post);
+    }
+}
+
+function intersectionCallback(entry) {
+    entry = entry[0]
+    let position = entry.boundingClientRect
+
+    if(-position.y > position.height) {
+        let el = entry.target
+        observer.unobserve(el)
+        appendMessage(el, "Post: Seen", "blue")
+        el.className += " seen";
     }
 }
 
@@ -25,7 +36,13 @@ function startTimer() {
 }
 
 function init() {
-    feed = document.querySelector('[id^="topnews_main_stream_"]')
+    let feed = document.querySelector('[id^="topnews_main_stream_"]')
+    const observerOptions = {
+        root: null,
+        rootMargin: "0px",
+        threshold: [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+    }
+    observer = new IntersectionObserver(intersectionCallback, observerOptions)
     appendMessage(feed, "Main Feed:", "black")
     markAsRead()
     startTimer()
