85c58ac9c55575f253c745d9bc803eedfd9181f2
Jan Matošík <jan.matosik@gmail.com>
2018-05-27 14:15:04 +0200

    Script refactored

--- myscript.js
+++ myscript.js
@@ -1,25 +1,3 @@
-function checkInitialPosts() {
-    const unobserved = document.querySelectorAll('[id^="hyperfeed_story_id_"]:not(.seen):not(.watching):not(.seen)')
-
-    for (let i = 0; i < unobserved.length; i++) {
-        const post = unobserved[i]
-        post.style.display = 'none'
-        isSeen(post)
-    }
-}
-
-function intersectionCallback(entry) {
-    entry = entry[0]
-    const position = entry.boundingClientRect
-
-    if(-position.y > position.height) {
-        const el = entry.target
-        setSeen(el.getAttribute('data-dedupekey'))
-        observer.unobserve(el)
-        el.classList.add('seen')
-    }
-}
-
 function initDexieDB() {
     const db = new Dexie("seen_posts")
     db.version(1).stores({
@@ -28,13 +6,107 @@ function initDexieDB() {
     return db
 }
 
+
+function setSeen(id, timestamp) {
+    db.posts.put({id: id, timestamp: timestamp})
+}
+
+
+function intersectionCallback(entry) {
+    entry = entry[0]
+    const position = entry.boundingClientRect
+
+    if(-position.y > position.height) {
+        const el = entry.target
+        setSeen(el.getAttribute('data-dedupekey'), el.getAttribute('data-timestamp'))
+        observer.unobserve(el)
+        el.classList.add('seen')
+    }
+}
+
+function waitForMainStream() {
+	return new Promise((resolve, reject) => {
+		const target = document.body
+		const config = { attributes: false, childList: true, characterData: false, subtree: true }
+		const observer = new MutationObserver(function(mutations, observer) {
+
+			mutations.forEach(function(mutation) {
+				if(mutation.target.querySelector('[id^="'+mainStreamIdPrefix+'"]') != null) {
+					console.log("mainstream observed!")
+					observer.disconnect()
+					resolve()
+				}
+			})
+		})
+		if (document.querySelector('[id^="'+mainStreamIdPrefix+'"]') != null) {
+			console.log("mainstream allready here")
+	       	resolve()
+	    }
+		observer.observe(target, config)
+	})
+}
+
+function waitForMainStreamRemove() {
+	return new Promise((resolve, reject) => {
+		const target = document.querySelector('[id^="'+mainStreamIdPrefix+'"]')
+		const config = { attributes: false, childList: true, characterData: false, subtree: true }
+		const observer = new MutationObserver(function(mutations, observer) {
+			mutations.forEach(function(mutation) {
+			    var nodes = Array.from(mutation.removedNodes)
+			    var match = nodes.some(parent => parent.contains(target))
+			    if (match) {
+			    	console.log("mainstream removed!")
+			      	observer.disconnect()
+			      	resolve()
+			    }
+
+			})
+		})
+		observer.observe(document.body, config)
+	})
+}
+
+function checkInitialPosts() {
+    const unobserved = document.querySelectorAll('[id^="hyperfeed_story_id_"]:not(.seen):not(.watching)')
+
+    for (let i = 0; i < unobserved.length; i++) {
+        const post = unobserved[i]
+        post.style.display = 'none'
+        isSeen(post)
+    }
+}
+
+function updateCounter() {
+    const counterElement = document.getElementById('seen-posts-notification-2423423')
+    counterElement.innerHTML = ++counter
+}
+
+function isSeen(post) {
+    const id = post.getAttribute('data-dedupekey')
+    if (id != null) {
+	    db.posts.get(id).then(function (row) {
+	        if (typeof row != 'undefined') {
+	            post.classList.add('seen')
+	            updateCounter()
+	        } else {
+	            post.style.display = ''
+	            if (! post.classList.contains('watching')) {
+	                post.classList.add('watching')
+	                observer.observe(post)
+	            }
+	        }
+	    })
+	} else {
+		console.log(post)
+	}
+}
+
 function initMutationObserver() {
-    const target = document.querySelector('[id^="topnews_main_stream_"]')
-    const config = { attributes: true, childList: true, characterData: true, subtree: true }
-    const mutationObserver = new MutationObserver(function(mutations) {
+    const target = document.querySelector('[id^="'+mainStreamIdPrefix+'"]')
+    const config = { attributes: true, childList: true, characterData: false, subtree: true, attributeFilter: ["id"]}
+    mutationObserver = new MutationObserver(function(mutations) {
         mutations.forEach(function(mutation) {
-            
-            if (mutation.type == 'childList' && mutation.target.id.includes('hyperfeed_story_id')) {
+            if (mutation.target.id.includes('hyperfeed_story_id')) {
                 mutation.target.style.display = 'none'
                 isSeen(mutation.target)
             }
@@ -53,40 +125,31 @@ function addNotificationBar() {
     feed.style = 'padding-bottom: ' + screen.height + 'px'
 }
 
-function updateCounter() {
-    const counterElement = document.getElementById('seen-posts-notification-2423423')
-    counterElement.innerHTML = ++counter
-}
-
-function init() {
-    db = initDexieDB()
-    observer = new IntersectionObserver(intersectionCallback, {threshold: [ 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0 ]})
-    initMutationObserver()
-    checkInitialPosts()
-    counter = 0
+function start() {
+	console.log("Starting!")
+	running = true
+	checkInitialPosts()
+	initMutationObserver()
+	counter = 0
     addNotificationBar()
 }
 
-function setSeen(id) {
-    db.posts.put({id: id})
+async function startLoop() {
+	setTimeout( async function() { 
+		if (running == true) {
+			await waitForMainStreamRemove()
+			mutationObserver.disconnect()
+		}
+		await waitForMainStream()
+		start()
+		startLoop()
+	}, 100)
 }
 
-function isSeen(post) {
-    const id = post.getAttribute('data-dedupekey')
-    db.posts.get(id).then(function (row) {
-        if (typeof row != 'undefined') {
-            post.classList.add('seen')
-            updateCounter()
-        } else {
-            post.style.display = ''
-            if (! post.classList.contains('watching')) {
-                post.classList.add('watching')
-                observer.observe(post)
-            }
-        }
-    })
-}
+mainStreamIdPrefix = "topnews_main_stream_"
+db = initDexieDB()
+observer = new IntersectionObserver(intersectionCallback, {threshold: [ 0.0, 0.2, 0.4, 0.6, 0.8, 1.0 ]})
 
-window.onload = function() {
-    init()
-}
+running = false
+startLoop()
+
